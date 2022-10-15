0d5f00c429dd9212b4a7957d024d1374b8600eb3
Minsik Jeong <minsigi0627@gmail.com>
2022-10-16 03:58:51 +0900

    babo added!

---
+++ AppBabo.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {type PropsWithChildren} from 'react';
+import {Text, View} from 'react-native';
+
+export default function AppBabo() {
+  return (
+    <View>
+      <Text>{'Hello baboboabo!!'}</Text>
+    </View>
+  );
+}
