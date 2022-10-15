0972fcbffcce10f1eac2766c909f6b94a3e9f657
Minsik Jeong <minsigi0627@gmail.com>
2022-10-16 02:43:59 +0900

    new! from minsigi0627

---
+++ AppNew.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {type PropsWithChildren} from 'react';
+import {Text, View} from 'react-native';
+
+export default function AppNew() {
+  return (
+    <View>
+      <Text>{'Hello new!!'}</Text>
+    </View>
+  );
+}
